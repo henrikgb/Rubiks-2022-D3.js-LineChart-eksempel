79cca41a64fef52fd825ea120244115288d06bdd
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2022-05-28 09:41:43 +0200

    Isolate dummy data in separate .js file

---
+++ dummyData.js
@@ -0,0 +1,516 @@
+const temperature = [
+    {
+        "date": "01/01/2021",
+        "value": 1,
+        "minValue": -2,
+        "maxValue": 3,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "11/01/2021",
+        "value": -3,
+        "minValue": -4,
+        "maxValue": -1,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "18/01/2021",
+        "value": -1,
+        "minValue": -3,
+        "maxValue": 0,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "25/01/2021",
+        "value": 4,
+        "minValue": 3,
+        "maxValue": 5,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "01/02/2021",
+        "value": -1,
+        "minValue": -2,
+        "maxValue": 0,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "08/02/2021",
+        "value": -5,
+        "minValue": -8,
+        "maxValue": -2,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "15/02/2021",
+        "value": -7,
+        "minValue": -8,
+        "maxValue": -3,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "22/02/2021",
+        "value": 8,
+        "minValue": 4,
+        "maxValue": 12,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "01/03/2021",
+        "value": 3,
+        "minValue": 0,
+        "maxValue": 7,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "08/03/2021",
+        "value": 5,
+        "minValue": 3,
+        "maxValue": 7,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "15/03/2021",
+        "value": 4,
+        "minValue": 0,
+        "maxValue": 8,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "22/03/2021",
+        "value": 7,
+        "minValue": 4,
+        "maxValue": 8,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "29/03/2021",
+        "value": 8,
+        "minValue": 7,
+        "maxValue": 9,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "05/04/2021",
+        "value": 1,
+        "minValue": 0,
+        "maxValue": 7,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "12/04/2021",
+        "value": 3,
+        "minValue": -1,
+        "maxValue": 5,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "19/04/2021",
+        "value": 11,
+        "minValue": 4,
+        "maxValue": 17,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "26/04/2021",
+        "value": 8,
+        "minValue": 3,
+        "maxValue": 12,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "03/05/2021",
+        "value": 8,
+        "minValue": 2,
+        "maxValue": 12,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "10/05/2021",
+        "value": 12,
+        "minValue": 7,
+        "maxValue": 18,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "17/05/2021",
+        "value": 9,
+        "minValue": 5,
+        "maxValue": 12,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "24/05/2021",
+        "value": 12,
+        "minValue": 8,
+        "maxValue": 19,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "07/06/2021",
+        "value": 18,
+        "minValue": 13,
+        "maxValue": 22,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "14/06/2021",
+        "value": 12,
+        "minValue": 8,
+        "maxValue": 15,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "21/06/2021",
+        "value": 16,
+        "minValue": 9,
+        "maxValue": 20,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "28/06/2021",
+        "value": 18,
+        "minValue": 14,
+        "maxValue": 24,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "05/07/2021",
+        "value": 18,
+        "minValue": 14,
+        "maxValue": 28,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "12/07/2021",
+        "value": 16,
+        "minValue": 14,
+        "maxValue": 26,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "19/07/2021",
+        "value": 14,
+        "minValue": 10,
+        "maxValue": 18,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "26/07/2021",
+        "value": 20,
+        "minValue": 18,
+        "maxValue": 28,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "02/08/2021",
+        "value": 14,
+        "minValue": 8,
+        "maxValue": 16,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "09/08/2021",
+        "value": 18,
+        "minValue": 16,
+        "maxValue": 23,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "16/08/2021",
+        "value": 15,
+        "minValue": 12,
+        "maxValue": 17,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "23/08/2021",
+        "value": 14,
+        "minValue": 11,
+        "maxValue": 17,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "30/08/2021",
+        "value": 14,
+        "minValue": 10,
+        "maxValue": 15,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "06/09/2021",
+        "value": 14,
+        "minValue": 10,
+        "maxValue": 15,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "13/09/2021",
+        "value": 16,
+        "minValue": 12,
+        "maxValue": 20,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "20/09/2021",
+        "value": 12,
+        "minValue": 8,
+        "maxValue": 16,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "27/09/2021",
+        "value": 12,
+        "minValue": 9,
+        "maxValue": 18,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "04/10/2021",
+        "value": 12,
+        "minValue": 9,
+        "maxValue": 18,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "11/10/2021",
+        "value": 8,
+        "minValue": 4,
+        "maxValue": 10,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "18/10/2021",
+        "value": 7,
+        "minValue": 4,
+        "maxValue": 10,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "25/10/2021",
+        "value": 9,
+        "minValue": 5,
+        "maxValue": 10,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "01/11/2021",
+        "value": 8,
+        "minValue": 4,
+        "maxValue": 10,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "08/11/2021",
+        "value": 8,
+        "minValue": 4,
+        "maxValue": 10,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "15/11/2021",
+        "value": 8,
+        "minValue": 4,
+        "maxValue": 10,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "22/11/2021",
+        "value": 3,
+        "minValue": 1,
+        "maxValue": 5,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "29/11/2021",
+        "value": 1,
+        "minValue": -4,
+        "maxValue": 4,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "06/12/2021",
+        "value": 1,
+        "minValue": -4,
+        "maxValue": 3,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "13/12/2021",
+        "value": 5,
+        "minValue": 1,
+        "maxValue": 6,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "20/12/2021",
+        "value": -3,
+        "minValue": -5,
+        "maxValue": 0,
+        "unit": "deg C",
+        "description": "Temperatur"
+    },
+    {
+        "date": "31/12/2021",
+        "value": 4,
+        "minValue": 2,
+        "maxValue": 6,
+        "unit": "deg C",
+        "description": "Temperatur"
+    }
+]
+const rainfall = [
+    {
+        "date": "01/01/2021",
+        "value": 86,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/02/2021",
+        "value": 94,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/03/2021",
+        "value": 100,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/04/2021",
+        "value": 53,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/05/2021",
+        "value": 49,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/06/2021",
+        "value": 67,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/07/2021",
+        "value": 139,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/08/2021",
+        "value": 86,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/09/2021",
+        "value": 68,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/10/2021",
+        "value": 214,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/11/2021",
+        "value": 170,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "01/12/2021",
+        "value": 62,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    },
+    {
+        "date": "31/12/2021",
+        "value": 65,
+        "minValue": 0,
+        "maxValue": 250,
+        "unit": "mm",
+        "description": "Nedbør"
+    }
+]
